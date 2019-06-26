d13b292202505354f937cad080d8392031801f4a
ejiroifie <ejiroifie@gmail.com>
2019-06-26 17:15:13 -0400

    rendering users entered list to existing list

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 addItemToList = () => {
-    var todoTitle = document.getElementById("todoInput").value;
-    console.log(todoTitle);
+    var todoTitle = document.getElementById("todoInput").value;  // catch the bus
+    console.log(todoTitle);  //catch the bus
 
-    var li =document.createElement("li");
-    var listText= document.createTextNode(todoTitle);
+    var li =document.createElement("li");  //li =<li></li>
+    var listText= document.createTextNode(todoTitle);  // catch the bus
     console.log(listText);
-    li.appendChild(listText);
+    li.appendChild(listText); //li =<li> "catch the bus"
+
+    document.getElementById("todo-list-items").appendChild(li);
 
 };
-
-
-
-
-
-
 
 
 // funcion addItemToList () {
