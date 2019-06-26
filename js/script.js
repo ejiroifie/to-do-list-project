94e3475d6b0d3bbbb6ee9a7110b79d21b1281174
ejiroifie <ejiroifie@gmail.com>
2019-06-26 19:28:51 -0400

    learned other useful things as by product. like exploring of objects and playing with them

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+// function when add button gets clicked
+
 addItemToList = () => {
     var todoTitle = document.getElementById("todoInput").value;  // catch the bus
     console.log(todoTitle);  //catch the bus
@@ -12,7 +15,55 @@ addItemToList = () => {
 
 };
 
+//Add Event listener for done items
+var list=document.getElementById("todo-list-items");
+document.addEventListener (
+    "click", 
+    function (ev)  {
 
-// funcion addItemToList () {
-//     alert("Your function worked");
-// }
+        // variables to get x and y cordinates
+    var xCordinate =ev.x;
+    var yCordinate =ev.y;
+
+    //creating text nodes with those cordinates as values
+    var xText= document.createTextNode(xCordinate);
+    var yText= document.createTextNode(yCordinate);
+
+    //text in paragraph to let users know about x and y cordinate
+    var currentXCordinateText =document.createTextNode  (
+        " Your current X cordinate: "
+    );
+
+
+    var currentYCordinateText =document.createTextNode  (
+        " Your current Y cordinate: "
+    );
+
+    // creating paragraph element for x cordinate
+    var p =document.createElement("p");
+    p.appendChild(currentXCordinateText);
+    p.appendChild(xText);
+
+    //appending paragraph to correct div element
+    document.getElementById ("current-cordinates").appendChild(p);
+
+    // creating paragraph element for y cordinate
+    var p =document.createElement("p");
+    p.appendChild(currentYCordinateText);
+    p.appendChild(yText);
+
+    // appending paragraph to correct div element
+    document.getElementById ("current-cordinates").appendChild(p);
+
+    alert(xCordinate + " " + yCordinate);
+    },
+    
+false
+);
+
+
+
+
+
+
+
